b0fcbec6aa65c0f1d158683fcbd8327570700a13
Deep-09 <deepbhasme@gmail.com>
2018-01-15 11:15:14 +0530

    Initial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=maggi-9c33-Web-Deploy.pubxml -p:Password=LKWlr3mJcr1CQc0nDlKACc8iLNt1u0n4p2iPlYWcST4xWZFoTyPawkYCyBt6
+
